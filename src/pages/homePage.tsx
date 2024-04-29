3aa62feff0aaf0a936e9f43b249d2ebe880698cf
Edward997 <edward.navarrete7@gmail.com>
2024-04-29 12:10:05 -0600

    Footer implemented

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -4,17 +4,14 @@ import React from 'react';
 import { Box, Heading, Text, Button, ChakraProvider } from '@chakra-ui/react'; // Importa componentes de Chakra UI según sea necesario
 import Navbar from '../components/navbar';
 import Banner from '../components/banner';
+import Footer from '../components/footer';
 
 const HomePage: React.FC = () => {
     return (
         <ChakraProvider>
             <Banner/>
             
-            <Box textAlign="center" mt={8}>
-                <Heading as="h1" size="xl" mb={4}>Bienvenido a mi sitio</Heading>
-                <Text fontSize="lg" mb={4}>Esta es la página de inicio de mi aplicación.</Text>
-                <Button colorScheme="blue" size="lg">Comenzar</Button>
-            </Box>
+            <Footer/>
         </ChakraProvider>
 
     );
